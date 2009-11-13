caf875f0650fd0a46ce09ab86839baaa17facffa
Myron Marston <myron.marston@gmail.com>
2009-11-13 14:21:15 -0800

    Updated the logic to follow redirects.  For example, when getting the favicon for http://linkedin.com, we need to first follow the redirect to http://www.linkedin.com, then parse that page for the favicon link.

--- spec/www-favicon_spec.rb
+++ spec/www-favicon_spec.rb
@@ -3,6 +3,7 @@ require 'ostruct'
 $LOAD_PATH << File.dirname(__FILE__) + '/../lib/'
 
 require 'www/favicon'
+require 'fakeweb'
 
 describe WWW::Favicon do
   before do
@@ -23,19 +24,24 @@ describe WWW::Favicon do
 
     it "should find from url" do
       @htmls.each do |html|
-        @favicon.should_receive(:request).and_return expectaction(:body => html)
+        @favicon.should_receive(:request).and_return expectaction(:body => html, :request_url => 'http://example.com')
         @favicon.find('http://example.com/').should == 'http://example.com/foo/favicon.ico'
       end
     end
-    
+
     it "should find from html and url" do
       @htmls.each do |html|
         @favicon.find_from_html(html, 'http://example.com/').should == 'http://example.com/foo/favicon.ico'
       end
     end
+
+    it "should use the request_url of the returned response to construct the favicon url for relative paths" do
+      @favicon.should_receive(:request).and_return expectaction(:body => '<html><link rel="Shortcut Icon" href="/foo/favicon.ico" /></html>', :request_url => 'http://images.example.com')
+      @favicon.find('http://example.com/').should == 'http://images.example.com/foo/favicon.ico'
+    end
   
     it "should find from default path" do
-      @favicon.should_receive(:request).and_return(expectaction(:body => '<html></html>'))
+      @favicon.should_receive(:request).and_return(expectaction(:body => '<html></html>', :request_url => 'http://www.example.com'))
       @favicon.find('http://www.example.com/').should == 'http://www.example.com/favicon.ico'
 
       @favicon.should_not_receive(:request)
@@ -43,13 +49,13 @@ describe WWW::Favicon do
     end
 
     it "should validate url" do
-      @favicon.stub!(:request).and_return(expectaction(:body => '<html></html>'))
+      @favicon.stub!(:request).and_return(expectaction(:body => '<html></html>', :request_url => 'http://www.example.com'))
       @favicon.should_receive(:valid_favicon_url?)
       @favicon.find('http://www.example.com/')
     end
 
     it "should return nil if #valid_favicon_url? returns false" do
-      @favicon.should_receive(:request).and_return(expectaction(:body => '<html></html>'))
+      @favicon.should_receive(:request).and_return(expectaction(:body => '<html></html>', :request_url => 'http://www.example.com'))
       @favicon.should_receive(:valid_favicon_url?).and_return(false)
       @favicon.find('http://www.example.com/').should be_nil
     end
@@ -80,25 +86,37 @@ describe WWW::Favicon do
       @response.content_type = 'application/xml'
       @favicon.valid_favicon_url?(@url).should == false
     end
+  end
 
-    it 'should follow redirect' do
-      @favicon.stub!(:request).and_return(@response)
-      @response.should_receive(:kind_of?).and_return(true)
-      @response.should_receive(:'[]').with('Location').and_return('http://example.com/foo/favicon.ico')
-      @response.should_receive(:kind_of?).and_return(false)
-
-      @favicon.valid_favicon_url?(@url).should == true
+  describe '#request' do
+    after(:each) do
+      FakeWeb.clean_registry
     end
 
-    it 'should return false if redirect limit is exceeded' do
-      @favicon.stub!(:request).and_return(@response)
+    it 'should return the response from the given url' do
+      FakeWeb.register_uri(:get, "http://example.com", :body => 'The response body')
+      @response = @favicon.send(:request, 'http://example.com')
+      @response.body.should == 'The response body'
+      @response.request_url.should == 'http://example.com'
+    end
 
-      10.times do |i|
-        @response.should_receive(:kind_of?).and_return(true)
-        @response.should_receive(:'[]').with('Location').and_return("http://example.com/#{i}/favicon.ico")
+    it 'should follow redirects' do
+      FakeWeb.register_uri(:get, "http://example.com", :status => ['301', 'Moved Permanently'], :location => 'http://www.example.com')
+      FakeWeb.register_uri(:get, "http://www.example.com", :body => 'The response body')
+      @response = @favicon.send(:request, 'http://example.com')
+      @response.body.should == 'The response body'
+      @response.request_url.should == 'http://www.example.com'
+    end
+
+    it 'should only follow up to 10 redirects' do
+      1.upto(12) do |i|
+        FakeWeb.register_uri(:get, "http://example.com/path#{i-1}", :status => ['301', 'Moved Permanently'], :location => "http://example.com/path#{i}")
       end
 
-      @favicon.valid_favicon_url?(@url).should == false
+      FakeWeb.register_uri(:get, "http://example.com/path11", :body => 'The response body')
+      @response = @favicon.send(:request, 'http://example.com/path0')
+      @response.should be_kind_of(Net::HTTPRedirection)
+      @response.request_url.should == 'http://example.com/path10'
     end
   end
 end
